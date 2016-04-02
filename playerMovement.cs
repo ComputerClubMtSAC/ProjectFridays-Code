b97b1d1b31f3cadd99cb3493885de0bfde7db8c6
ImThaDude <jorge_quan@hotmail.com>
2016-04-01 18:51:09 -0700

    First Commit
    This comes with the first piece of code for the Project::Fridays catch
    up page.

---
+++ playerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	public float movementModifier = 5f;
+	Vector3 direction;
+	CharacterController control;
+
+	// Use this for initialization
+	void Start () {
+
+		control = GetComponent<CharacterController>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		direction = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		direction.Normalize ();
+		control.Move (direction * movementModifier * Time.deltaTime);
+	
+	}
+}
